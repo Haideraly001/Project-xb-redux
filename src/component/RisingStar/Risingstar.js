efdbf3b857d30d1d48ec75f4f6a9c954f2f00748
Haider Aly <haideralymughal@gmail.com>
2024-10-16 23:14:14 +0500

    create once again

--- src/component/RisingStar/Risingstar.js
+++ src/component/RisingStar/Risingstar.js
@@ -7,43 +7,40 @@ const Risingstar = ({ maxRating }) => {
   const [star, setStar] = useState(0)
   const [hover, setHover] = useState(0)
 
-  const handleClick = (index) => {
-    setStar(index + 1)
-    console.log(index + 1);
+  const handleMouseEnter = (handleIndex) => {
+    setHover(handleIndex + 1)
+    console.log(handleIndex + 1);
 
   }
 
-  const handleMouseHover = (index) => {
-    setHover(index + 1)
-    console.log(index + 1);
-  }
-
-  const handleMouseLeave = () => {
+  const handleMouseLeave = (handleIndex) => {
     setHover(0)
     console.log(0);
 
   }
 
-
+  const handleClick = (handleIndex) => {
+    setStar(handleIndex + 1)
+    console.log(handleIndex + 1);
+  }
 
   return (
-    <div className="flex flex-col items-center border m-4 p-4">
-      <h3 className="text-4xl mb-4">Rising Star</h3>
-      <div className="flex space-x-2">
-        {
-          [...Array(maxRating)].map((_, index) => {
-            return <FontAwesomeIcon icon={faStar}
-              key={index}
-              onClick={() => handleClick(index)}
-              onMouseMove={() => handleMouseHover(index)}
-              onMouseLeave={() => handleMouseLeave(index)}
-              size='1x'
-              className={` cursor-pointer ${(hover || star) > index ? 'text-yellow-500' : 'text-gray'}`}
-            />
-          })
-        }
+    <div>
+      <h1>Rising Star</h1>
+      {
+        [...Array(maxRating)].map((_, index) => (
+          <FontAwesomeIcon
+            icon={faStar}
+            key={index}
+            className={` cursor-pointer ${(star || hover) > index ? 'text-yellow-300' : "text-slate-300"}`}
+            onMouseEnter={() => handleMouseEnter(index)}
+            onMouseLeave={() => handleMouseLeave(index)}
+            onClick={() => handleClick(index)}
+            style={{ fontSize: 40 }}
+          />
+        ))
 
-      </div>
+      }
     </div>
   )
 }
