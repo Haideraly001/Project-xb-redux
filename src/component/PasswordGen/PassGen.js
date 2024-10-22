1e508c87a88497a2a3b56d41dafa0a2e6586e9fc
Haider Aly <haideralymughal@gmail.com>
2024-10-22 20:36:38 +0500

    password generator

--- src/component/PasswordGen/PassGen.js
+++ src/component/PasswordGen/PassGen.js
@@ -1,10 +1,13 @@
-import React, { useCallback, useEffect, useState } from 'react'
+import React, { useCallback, useEffect, useRef, useState } from 'react'
 
 const PassGen = () => {
   const [length, setLength] = useState(8)
   const [numberAllow, setNumberAllow] = useState(false)
   const [charAllowed, setCharAllowed] = useState(false)
   const [password, setPassword] = useState("")
+
+  const passwordRef = useRef(null)
+
 
   const passwordGenretor = useCallback(() => {
     let str = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz'
@@ -25,8 +28,11 @@ const PassGen = () => {
     setPassword(pass)
 
 
-  }, [length, numberAllow, charAllowed, setPassword])
+  }, [length, numberAllow, charAllowed])
 
+  const copyPassword = useCallback(() => {
+    window.navigator.clipboard.writeText(password)
+  }, [password])
 
   useEffect(() => {
     passwordGenretor()
@@ -34,7 +40,7 @@ const PassGen = () => {
 
   return (
     <>
-      <div className="bg-gray-800 p-6 rounded-lg shadow-md max-w-md mx-auto ">
+      <div className="bg-gray-800 p-6 rounded-lg shadow-md max-w-lg mx-auto ">
         <h2 className="text-white text-center mb-4">Password generator</h2>
         <div className='flex items-center'>
           <input
@@ -43,7 +49,7 @@ const PassGen = () => {
             placeholder="Password"
             className="w-full px-4 py-2 text-gray-800 rounded-l-lg focus:outline-none"
           />
-          <button className='outline-none bg-blue-700 text-white px-3 py-2 gap-2 shrink-0 rounded-r-lg'>Copy</button>
+          <button className='outline-none bg-blue-700 text-white px-3 py-2 gap-2 shrink-0 rounded-r-lg' onClick={() => copyPassword()}>Copy</button>
         </div>
         <div className='flex text-sm gap-x-2 mt-3'>
           <div className='flex items-cneter gap-x-1'>
