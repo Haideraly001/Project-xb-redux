ea3f639f60b05e701af9544c912c4ead6b70f485
Haider Aly <haideralymughal@gmail.com>
2024-11-24 05:32:45 +0500

    working on redux toolkit

--- src/App.js
+++ src/App.js
@@ -1,27 +1,13 @@
 import React from 'react'
 import "./App.css"
-// import Risingstar from './component/RisingStar/Risingstar'
-import PassGen from './component/PasswordGen/PassGen'
-// import UseCallback from './component/Usecallback/UseCallback'
-// import YtAccordion from './component/YtAccordion/YtAccordion'
-// import UseEffect from './component/useEffect/UseEffect'
-// import ShoppingList from './component/ShoppingList'
-// import Accordien from './component/Accordian/Accordien'
+
 
 const App = () => {
   return (
     <div className='bg-blue-900 max-w-full min-h-screen'>
 
       <div className="pt-5" >
-        {/* <h1>Shopping List</h1>
-      <ShoppingList /> */}
-        {/* <Accordien /> */}
-        {/* <UseEffect /> */}
-        {/* <YtAccordion /> */}
-        {/* <UseCallback /> */}
-        <PassGen />
-        {/* <Risingstar maxRating={8} /> */}
-        {/* <Risingstar maxRating={5} /> */}
+        redux practice
       </div>
     </div >
   )
