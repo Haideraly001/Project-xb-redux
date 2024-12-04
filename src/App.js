5122d7d0c61d4f7a8863c3887f5391611ef1ea2d
Haider Aly <haideralymughal@gmail.com>
2024-12-04 19:35:23 +0500

    working with code bytes

--- src/App.js
+++ src/App.js
@@ -1,14 +1,11 @@
 import React from 'react'
 import './App.css'
-import QRcode from './projects/QRcode'
 
 
 const App = () => {
 
   return (
     <div className='App'>
-
-      <QRcode />
     </div>
   )
 }
