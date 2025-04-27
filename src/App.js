df2c179fca2517c5d22fc66e4dd06ba2c6fdf33a
haideraly001 <haideralymughal@gmail.com>
2025-04-27 09:25:33 -0700

    init

--- src/App.js
+++ src/App.js
@@ -1,16 +1,12 @@
-import React from 'react'
-import './App.css'
-// import ScrollIndecator from './projects/Scroll-indecator'
-
-
+import React from "react";
+import "./App.css";
 
 const App = () => {
-
   return (
-    <div className='App'>
-      {/* <ScrollIndecator url={'https://dummyjson.com/products?limit=100'} /> */}
+    <div className="App">
+      <h1>Scroll Indecator</h1>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
