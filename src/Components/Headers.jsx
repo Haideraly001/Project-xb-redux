444382e77ada223d6b68e7e25e014e2fa99bec52
haideraly001 <haideralymughal@gmail.com>
2025-06-02 16:08:43 +0500

    project setup

---
+++ src/Components/Headers.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Headers = () => {
+  return (
+    <div>
+      <h1>headers</h1>
+    </div>
+  );
+};
+
+export default Headers;
---
+++ src/Page/Home.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Headers from "../Components/headers";
+import Categories from "../Components/Categories";
+import Projects from "../Components/projects";
+
+const Home = () => {
+  return (
+    <>
+      <Headers />
+      <Categories />
+      <Projects />
+    </>
+  );
+};
+
+export default Home;
