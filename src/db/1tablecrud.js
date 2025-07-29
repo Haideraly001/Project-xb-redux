bbb759fe4037326f64ba72813051e8433daa0171
haideraly001 <haideralymughal@gmail.com>
2025-07-29 18:37:30 +0500

    assign new task

--- src/db/1tablecrud.js
+++ src/db/1tablecrud.js
@@ -69,3 +69,11 @@ will practive tomorrow
 learn and read about the joins 
 inner joins, left joins, right joins, full outer joins
 again learn about aggregation group by, having by
+
+29 july 2025 to 31 july 2025
+
+practice all crud operations on the employees and personInfo tables
+practice all alter & table crud operations on the employees and personInfo tables
+practice all aggregation operations (sum, count, avg, max, min) on the employees and personInfo tables
+pratice all joins operations on the employees and personInfo tables
+
